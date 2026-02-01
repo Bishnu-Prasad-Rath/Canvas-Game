51ba475814767a9f4e033cc5a9331066175f5000
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-02-01 16:34:01 +0530

    Short clear message

--- Game-1/backend/middleware/auth.js
+++ Game-1/backend/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const SECRET = "my_secret_key";
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "No token" });
 
   try {
--- Game-1/backend/server.js
+++ Game-1/backend/server.js
@@ -6,20 +6,21 @@ const app = express();
 
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST"],
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/canvas_game");
+mongoose.connect("mongodb://127.0.0.1:27017/canvas_game")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/score", require("./routes/score"));
 app.use("/api/game", require("./routes/game"));
 
-
 app.listen(5000, "0.0.0.0", () => {
   console.log("Backend running on port 5000");
 });
-
